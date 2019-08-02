99c46fecc506b16cea47c12e268f7058665b284e
Apple <apple@Lila-MAC.local>
2019-08-02 11:56:25 -0700

    third commit

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var container = $(".container");
 button.on("click", save);
 
 function save(event){
-  var src = event.target.parentNode.children[0].src;
+  var src = event.target.parentNode.children[0].children[0].src;
+  console.log(src);
   var image = "<img src = \"" + src + "\">";
   console.log(src);
   container.append(image);
